dcc9015cac35788d0246cf7a0dc03ce4eb44c5a9
jazztinecruz <jzztn.crz@gmail.com>
2022-01-04 14:31:16 +0800

    update

--- pages/login.jsx
+++ pages/login.jsx
@@ -4,7 +4,7 @@ const Login = () => {
 	return (
 		<div className="h-full bg-login bg-cover bg-no-repeat grid justify-center items-center">
 			<div className="bg-black bg-opacity-30 p-8 grid grid-rows-[auto,1fr,auto] gap-8 rounded-xl">
-				<div className="">
+				<div>
 					<h1 className="text-white text-center text-3xl">"Always by your Side"</h1>
 				</div>
 
@@ -13,8 +13,17 @@ const Login = () => {
 					<input placeholder="Password" className="mt-4 border-none outline-none w-full py-4 pl-3 rounded-md placeholder:text-opacity-20" />
 				</div>
 
-				<Link href={'/student/profile'}><button className="bg-button py-4 text-white text-md rounded-md tracking-wider hover:font-semibold transition-all duration-500">LOG IN</button></Link>
-				<Link href={'/signup'}><span className="text-white text-sm text-center tracking-wide opacity-75 cursor-pointer hover:text-button transition-colors duration-300">Don't have an account?</span></Link>
+				<Link href={'/student/profile'}>
+					<button className="bg-button py-4 text-white text-md rounded-md tracking-wider hover:font-semibold transition-all duration-500">
+						LOG IN
+					</button>
+				</Link>
+
+				<Link href={'/signup'}>
+					<span className="text-white text-sm text-center tracking-wide opacity-75 cursor-pointer hover:text-button transition-colors duration-300">
+						Create an Account
+					</span>
+				</Link>
 			</div>
 		</div>
 	)
