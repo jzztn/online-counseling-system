0ab9381a7d9d1b32570d29964614bf8fdbf7fde9
jazztinecruz <jzztn.crz@gmail.com>
2022-01-06 13:42:03 +0800

    updated

--- components/AppointmentCard.tsx
+++ components/AppointmentCard.tsx
@@ -5,18 +5,18 @@ const AppointmentCard = ({ profile, name, degree, degree2, sched, sched2 }: any)
 				<img src={profile} alt="profile" className="w-20 h-20 lg:w-24 lg:h-24 mx-auto rounded-full" />
 			</div>
 
-			<div className="flex flex-col mt-8">
+			<div className="flex flex-col mt-4 lg:mt-8">
 				<h1 className="text-xl font-semibold tracking-wide mb-6">{name}</h1>
 				<h1 className="text-sm font-light">{degree}</h1>
 				<h1 className="text-sm font-light mt-2">{degree2}</h1>
 			</div>
 
 			<div className="flex flex-col gap-2">
-				<h1 className="text-sm font-light mt-10">{sched}</h1>
+				<h1 className="text-sm font-light mt-4 lg:mt-10">{sched}</h1>
 				<h1 className="text-sm font-light">{sched2}</h1>
 			</div>
 
-			<button className="mt-8 py-5 bg-button rounded-sm text-white text-sm tracking-wide hover:font-semibold transition-colors duration-200"> SET APPOINTMENT</button>
+			<button className="mt-8 py-5 bg-button rounded-sm text-white text-xs tracking-wide hover:font-semibold transition-colors duration-200"> SET APPOINTMENT</button>
 		</div>
 	)
 }
